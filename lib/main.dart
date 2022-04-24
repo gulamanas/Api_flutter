8a065014c5debecffa81b252f1a6063dd12ebe73
Gulam Anas <g.anas786@gmail.com>
2022-04-24 13:19:55 +0530

    data fetched successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart';
+import 'package:intl/intl.dart';
 
 void main() async {
   Map _data = await getData();
 
-  print(_data);
+  List _features = _data['features'];
+
+  // print(_data);
 
   runApp(MaterialApp(
     home: Scaffold(
@@ -14,6 +17,29 @@ void main() async {
           'Quake App',
         ),
         centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: _features.length,
+        itemBuilder: (BuildContext context, int position) {
+          if (position.isOdd) return new Divider();
+          final index = position ~/2; // we are dividing position by 2 and returning an integer result
+
+          var format = DateFormat.yMMMMd("en_US").add_jm();
+          // print(format);
+
+          var date = format.format(DateTime.fromMicrosecondsSinceEpoch(_features[index]['properties']['time'] * 1000, isUtc: true ) );
+          // print(_features);
+          print(date);
+
+
+
+          return ListTile(
+            title: Text("$date"),
+            subtitle: Text(
+              "${_features[index]["properties"]["place"]}"
+            ),
+          );
+        }
       ),
       ),
   ));
